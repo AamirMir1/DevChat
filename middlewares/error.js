30a1fd1c94aaad2477413c4f89aa142ff39968b4
Aamir Mir <aamir72633@gmail.com>
2025-01-12 16:22:08 +0500

    Some changes in error middleware file

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,8 @@
 import { envMode } from "../app.js";
 
 const errorMiddleware = (err, req, res, next) => {
-  err.message ||= "Internal Server Error";
-  err.statusCode ||= 500;
+  err.message = err?.message || "Internal Server Error";
+  err.statusCode = err?.statusCode || 500;
 
   if (err.code === 11000) {
     const error = Object.keys(err.keyPattern).join(",");
